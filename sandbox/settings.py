a499530e8602e7860224073713d64d27d713f64c
Kees Hink <keeshink@gmail.com>
2018-01-11 13:39:38 +0100

    Only send confirmation mail if order status is paid

--- sandbox/settings.py
+++ sandbox/settings.py
@@ -1,18 +1,20 @@
 from settings import *
 
 
+ORDER_STATUS_PAID = 'Being processed'
+
 # Oscar Shop settings
 OSCAR_INITIAL_ORDER_STATUS = OSCAR_INITIAL_LINE_STATUS = 'Pending Payment'
 OSCAR_ORDER_STATUS_PIPELINE = {
-    'Pending Payment': ('Being processed', 'Cancelled',),
-    'Being processed': ('Being processed', 'Cancelled',),
+    'Pending Payment': (ORDER_STATUS_PAID, 'Cancelled',),
+    ORDER_STATUS_PAID: (ORDER_STATUS_PAID, 'Cancelled',),
     'Cancelled': (),
 }
 
 # Mollie settings
 MOLLIE_API_KEY = 'secret-key-123'
 MOLLIE_STATUS_MAPPING = {
-    'Paid': 'Being processed',
+    'Paid': ORDER_STATUS_PAID,
     'Pending': 'Pending Payment',
     'Open': 'Pending Payment',
     'Cancelled': 'Cancelled'
